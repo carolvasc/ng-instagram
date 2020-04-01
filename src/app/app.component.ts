49e5edf109dcc4838ff3fde5186db7e456ad75c9
Carol Vasconcelos <cvsouza04@gmail.com>
2020-03-31 22:44:38 -0300

    Configuração do firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-instagram';
+
+  ngOnInit() {
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyBYCzo1kOyy_mqW_a-usKhgrHsh9ezPWss",
+      authDomain: "ng-instagram-b742b.firebaseapp.com",
+      databaseURL: "https://ng-instagram-b742b.firebaseio.com",
+      projectId: "ng-instagram-b742b",
+      storageBucket: "ng-instagram-b742b.appspot.com",
+      messagingSenderId: "257353096204",
+      appId: "1:257353096204:web:fb73acfbec20b4299fa229",
+      measurementId: "G-WF2XD8G2BD"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
 }
