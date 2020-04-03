9b26878b2e4e583149db2c0b4b19c7c4c0926f28
Carol Vasconcelos <carol.vasconcelos080@gmail.com>
2020-04-02 23:45:57 -0300

    WIP: Formulario de login

--- src/app/access/login/login.component.ts
+++ src/app/access/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { Subject } from 'rxjs';
 
 @Component({
@@ -7,16 +8,32 @@ import { Subject } from 'rxjs';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  get email() { return this.loginForm.get('email').value };
+  get password() { return this.loginForm.get('password').value };
 
   @Output() showRegister: Subject<boolean> = new Subject<boolean>();
 
   constructor() { }
 
   ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm() {
+    this.loginForm = new FormGroup({
+      'email': new FormControl(null),
+      'password': new FormControl(null)
+    })
   }
 
   showRegisterForm() {
     this.showRegister.next(true);
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value)
   }
 
 }
