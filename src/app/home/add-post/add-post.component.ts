74b3c8caa035cf82b7fb9ee89a440975dc75591c
Carol Vasconcelos <cvsouza04@gmail.com>
2020-04-08 00:27:01 -0300

    WIP: Progresso do upload

--- src/app/home/add-post/add-post.component.ts
+++ src/app/home/add-post/add-post.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import { HomeService } from '../home.service';
+
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+
+import { HomeService } from '../home.service';
+import { ProgressService } from 'src/app/progress.service';
+
+import { interval, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-post',
@@ -17,7 +23,7 @@ export class AddPostComponent implements OnInit {
   email: string = null;
   image: any = null;
 
-  constructor(private homeService: HomeService) { }
+  constructor(private homeService: HomeService, private progressService: ProgressService) { }
 
   ngOnInit(): void {
     this.createForm();
@@ -34,6 +40,23 @@ export class AddPostComponent implements OnInit {
 
   onSubmit() {
     this.homeService.createPost({ email: this.email, title: this.title, image: this.image[0] });
+
+    const { status, state } = this.progressService;
+
+    let upload = interval(1500);
+
+    let inProgress = new Subject();
+    inProgress.next(true);
+
+    upload
+      .pipe(takeUntil(inProgress))
+      .subscribe(() => {
+        console.log(state);
+        console.log(status);
+
+        if (status === 'done')
+          inProgress.next(false);
+      });
   }
 
   prepareImageUpload(event: Event) {
--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -8,12 +8,14 @@ import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/storage';
 
+import { ProgressService } from '../progress.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HomeService {
 
-  constructor() { }
+  constructor(private progressService: ProgressService) { }
 
   createPost({ email, title, image }) {
     const md5 = new Md5();
@@ -22,7 +24,15 @@ export class HomeService {
 
     firebase.storage().ref()
       .child(`images/${fileName}`)
-      .put(image);
+      .put(image)
+      .on(firebase.storage.TaskEvent.STATE_CHANGED,
+        snapshot => {
+          this.progressService.status = 'progress';
+          this.progressService.state = snapshot;
+        },
+        error => this.progressService.status = 'error',
+        () => this.progressService.status = 'done'
+      )
 
     // firebase.database().ref(`posts/${btoa(email)}`)
     //   .push({ title: title });
